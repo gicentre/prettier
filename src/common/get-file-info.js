7b75b51f805b8673f5f1ef2243a807cfdffd72e6
Alexander Kachkaev <alexander@kachkaev.ru>
2018-05-07 18:14:59 +0100

    Rename filepath to filePath in getFileInfo for consistency with other API methods

--- src/common/get-file-info.js
+++ src/common/get-file-info.js
@@ -4,15 +4,15 @@ const createIgnorer = require("../common/create-ignorer");
 const options = require("../main/options");
 
 /**
- * @param {string} filepath
+ * @param {string} filePath
  * @param {{ ignorePath?: string, withNodeModules?: boolean, plugins: object }} opts
  */
-function getFileInfo(filepath, opts) {
+function getFileInfo(filePath, opts) {
   let ignored = false;
   const ignorer = createIgnorer(opts.ignorePath, opts.withNodeModules);
-  ignored = ignorer.ignores(filepath);
+  ignored = ignorer.ignores(filePath);
 
-  const inferredParser = options.inferParser(filepath, opts.plugins) || null;
+  const inferredParser = options.inferParser(filePath, opts.plugins) || null;
 
   return {
     ignored,
